801bc142f73a3a37980f0495b9337ea5acf95586
koru07478-cpu <koru07478@gmail.com>
2026-08-31 21:03:02 +0300

    Параметры видео поменяла, чтобы было меньше шума

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ if success:
 else:
     print("Не удалось сохранить кадр")
 
-capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
 mask = cv2.imread("roi_mask.png", cv2.IMREAD_GRAYSCALE)
 if mask is None:
     raise RuntimeError("Не удалось открыть ROI-маску")
@@ -22,6 +20,8 @@ _, mask = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
 height, width = frame.shape[:2]
 if mask.shape != (height, width):
     raise RuntimeError(f"Размер маски {mask.shape} не совпадает с размером кадра {(height, width)}")
+
+capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
 try:
     fps = capture.get(cv2.CAP_PROP_FPS)
@@ -33,14 +33,17 @@ try:
             break
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        adjusted = cv2.convertScaleAbs(gray, alpha=1.5, beta=30)
-        blurred = cv2.GaussianBlur(adjusted, (5, 5), 0)
+        adjusted = cv2.convertScaleAbs(gray, alpha=1.5, beta=-50)
+        blurred = cv2.GaussianBlur(adjusted, (13, 13), 0)
         edges = cv2.Canny(blurred, 50, 150)
+
+# Можно еще от цвета полос отличить (из-за всяких теней деревьев или машин)
 
         roi_edges = cv2.bitwise_and(edges, mask)
 
         cv2.imshow("Original", frame)
-        cv2.imshow("Edges", edges)
+        cv2.imshow("Edges1", adjusted)
+        cv2.imshow("Edges2", edges)
         cv2.imshow("ROI Edges", roi_edges)
 
         if cv2.waitKey(delay_ms) & 0xFF == ord("q"):
